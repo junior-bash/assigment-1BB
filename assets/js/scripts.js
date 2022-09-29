fd8ea40f3533ca7ec9cf9432b00b658a4c707217
junior-bash <bobkalala12@gmail.com>
2022-09-29 13:35:34 +0200

    On time as promised. 1D

---
+++ assets/js/scripts.js
@@ -0,0 +1,19 @@
+/*!
+* Start Bootstrap - Bare v5.0.8 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+function myCreateFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+  }
+  
+  function myDeleteFunction() {
+    document.getElementById("myTable").deleteRow(1);
+  }
